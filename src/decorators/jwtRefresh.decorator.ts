dbdb0b3999aa58715d0d3d0ed60e1aec0d297567
Yohai Ido <yohai@networx.com>
2023-08-27 12:57:02 +0300

    add jet test

--- src/decorators/jwtRefresh.decorator.ts
+++ src/decorators/jwtRefresh.decorator.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 
 import { IApiClient, RequestMethods, RequestOptions } from '../allTypes';
 import { Decorator } from '../decorators/base.decorator';
@@ -9,28 +9,36 @@ interface IRefreshCallback {
 
 export class JWTRefreshDecorator extends Decorator {
   private refreshCallback: IRefreshCallback;
+  private defaultHeaders: any;
 
-  constructor(apiClient: IApiClient, params: { refreshCallback: IRefreshCallback }, baseClient: any) {
+  constructor(
+    apiClient: IApiClient,
+    params: { refreshCallback: IRefreshCallback },
+    public baseClient: any,
+  ) {
     super(apiClient);
+    this.baseClient = baseClient;
     this.refreshCallback = params.refreshCallback;
   }
 
   async all<T = any>(method: RequestMethods, options: RequestOptions): Promise<T> {
     try {
+      console.log('JWTRefreshDecorator.all');
       return await this.apiClient[method](options);
     } catch (error) {
       const err = error as AxiosError;
 
       if (err.response && err.response.status === 401) {
         const newToken = await this.refreshCallback();
-        options.headers = {
-          ...options.headers,
-          Authorization: `Bearer ${newToken}`,
-        };
+        this.baseClient.instance.defaults.headers.common.Authorization = newToken;
         return await this.apiClient[method](options);
       } else {
         throw error;
       }
     }
+  }
+
+  cleanup(): void {
+    delete this.baseClient.instance.defaults.headers.common.Authorization;
   }
 }
--- src/decorators/tests/JWTRefreshDecorator.test.ts
+++ src/decorators/tests/JWTRefreshDecorator.test.ts
@@ -1,20 +1,17 @@
-import { AxiosResponse } from 'axios';
-
 import { MockAxiosBaseApiClient } from '../../../tests/mockBaseApiClient';
-import { RequestOptions } from '../../allTypes'; // Adjust the import based on your file structure
 import { ApiClient } from '../../api/apiClient';
 import { JWTRefreshDecorator } from '../jwtRefresh.decorator';
 
 describe('JWTRefreshDecorator', () => {
-  it('should refresh token on 401 and retry', async () => {
-    const apiClient = new ApiClient(
+  let apiClient: any, mockRefreshCallback: any, mock401Response: any, mockResponse: any, options: any;
+
+  beforeEach(() => {
+    apiClient = new ApiClient(
       new MockAxiosBaseApiClient('http://localhost:3000', {
         get: { data: [{ id: 1, title: 'foo' }] },
       }),
     );
-
-    const mockRefreshCallback = jest.fn();
-
+    mockRefreshCallback = jest.fn();
     apiClient.addDecorator({
       decorator: JWTRefreshDecorator,
       params: {
@@ -22,7 +19,7 @@ describe('JWTRefreshDecorator', () => {
       },
     });
 
-    const mock401Response: AxiosResponse = {
+    mock401Response = {
       data: 'Unauthorized',
       status: 401,
       statusText: 'Unauthorized',
@@ -30,7 +27,7 @@ describe('JWTRefreshDecorator', () => {
       config: {} as any,
     };
 
-    const mockResponse: AxiosResponse = {
+    mockResponse = {
       data: 'some data',
       status: 200,
       statusText: 'OK',
@@ -38,22 +35,64 @@ describe('JWTRefreshDecorator', () => {
       config: {} as any,
     };
 
-    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
-    apiClient.baseClient.instance.get.mockResolvedValueOnce(mockResponse);
-    mockRefreshCallback.mockResolvedValueOnce('new-token');
-    const options: RequestOptions = {
+    options = {
       url: '/some/url',
     };
-    const response = await apiClient.get(options);
+  });
 
+  it('should refresh token on 401', async () => {
+    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
+    mockRefreshCallback.mockResolvedValueOnce('Bearer new-token');
+
+    await apiClient.get(options);
+    expect(mockRefreshCallback).toBeCalledTimes(1);
+  });
+
+  it('should retry request after token refresh', async () => {
+    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
+    apiClient.baseClient.instance.get.mockResolvedValueOnce(mockResponse);
+    mockRefreshCallback.mockResolvedValueOnce('Bearer new-token');
+
+    const response = await apiClient.get(options);
     expect(response).toEqual(mockResponse);
-    expect(mockRefreshCallback).toBeCalled();
-    expect(apiClient.baseClient.instance.get).toBeCalledTimes(2);
-    expect(apiClient.baseClient.instance.get).toHaveBeenNthCalledWith(1, '/some/url', {});
-    expect(apiClient.baseClient.instance.get).toHaveBeenNthCalledWith(2, '/some/url', {
-      headers: {
-        Authorization: 'Bearer new-token',
-      },
+  });
+
+  it('should update headers on token refresh', async () => {
+    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
+    mockRefreshCallback.mockResolvedValueOnce('Bearer new-token');
+
+    const headerBefore = { ...apiClient.baseClient.instance.defaults.headers.common };
+    await apiClient.get(options);
+    const headerAfter = { ...apiClient.baseClient.instance.defaults.headers.common };
+
+    expect(headerBefore).toEqual({});
+    expect(headerAfter).toEqual({
+      Authorization: 'Bearer new-token',
     });
+  });
+
+  it('should use new token in subsequent requests', async () => {
+    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
+    mockRefreshCallback.mockResolvedValueOnce('Bearer new-token');
+    await apiClient.get(options);
+    await apiClient.get(options);
+
+    expect(apiClient.baseClient.instance.defaults.headers.common).toEqual({
+      Authorization: 'Bearer new-token',
+    });
+  });
+
+  it('should call methods correct number of times', async () => {
+    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
+    await apiClient.get(options); // 1 and return 401 and do again 2 and return 200
+    await apiClient.get(options); // 3 and return 200
+
+    mockRefreshCallback.mockResolvedValueOnce('Bearer new-token-2');
+    apiClient.baseClient.instance.get.mockRejectedValueOnce({ response: mock401Response });
+
+    await apiClient.get(options); // 4 and return 401 and do again 5 and return 200
+
+    expect(apiClient.baseClient.instance.get).toBeCalledTimes(5);
+    expect(mockRefreshCallback).toBeCalledTimes(2);
   });
 });
